9e3fb28242f2630cae4212c544d56d94be40174b
Thairam <thayram01@gmail.com>
2020-09-08 12:52:20 -0300

    Evento Adicionar

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -12,7 +12,7 @@ export default props => (
         </Grid>
 
         <Grid cols="12 3 2">
-            <IconButton style="primary" icon="plus" />
+            <IconButton onClick={props.handleAdd} style="primary" icon="plus" />
         </Grid>
 
     </div>
